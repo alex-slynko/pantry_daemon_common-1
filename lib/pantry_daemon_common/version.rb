08fcfeef203676f369c2a69d300aa95d08cc6fcd
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2014-02-10 17:19:49 +0000

    TD-1769

--- lib/pantry_daemon_common/version.rb
+++ lib/pantry_daemon_common/version.rb
@@ -1,3 +1,3 @@
 module PantryDaemonCommon
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
--- lib/wonga/daemon/aws_resource.rb
+++ lib/wonga/daemon/aws_resource.rb
@@ -7,9 +7,36 @@ module Wonga
         aws.instances[id]
       end
 
+      def stop(message)
+        instance = aws.instances[message['instance_id']]
+
+        unless instance.exists?
+          logger.error("ERROR: machine not found #{message["name"]} - request_id: #{message["id"]}")
+          return
+        end
+
+        case instance.status
+        when :stopped
+          logger.info("Stopped instance: #{message["name"]} - request_id: #{message["id"]} - stopped")
+          return true
+        when :terminated
+          logger.error("Attempted to stop terminated instance: #{message["name"]} - id #{message["id"]}")
+          return
+        when :running
+          logger.info("Stopping instance: #{message["name"]} - request_id: #{message["id"]} - stopping")
+          instance.stop
+        end
+
+        raise "Instance #{message['instance_id']} is stopping"
+      end
+
       private
       def aws
         @aws ||= AWS::EC2.new
+      end
+
+      def logger
+        Wonga::Daemon.logger
       end
     end
   end
