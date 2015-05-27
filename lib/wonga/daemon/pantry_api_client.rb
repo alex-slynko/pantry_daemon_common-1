589446562f5bc71e9e126bf4ee7839041f19ccce
Alex Slynko <slynko@outlook.com>
2015-05-27 18:03:12 +0100

    Skip using logger for RestClient
    Change-Id: Id95f8db26906e29e2fe479a1afbde53ab5dee5a3

--- lib/wonga/daemon/pantry_api_client.rb
+++ lib/wonga/daemon/pantry_api_client.rb
@@ -6,29 +6,29 @@ module Wonga
     class PantryApiClient
       def initialize(url, api_key, logger, timeout = 300)
         @resource = RestClient::Resource.new(url, timeout: timeout, headers: { accept: :json, content_type: :json, 'x-auth-token': api_key })
-        RestClient.log = logger
+        @logger = logger
       end
 
       def send_get_request(url)
         response = @resource[url].get.body
-        RestClient.log.info "GET request to #{url} was sent successfully"
-        RestClient.log.debug response
+        @logger.info "GET request to #{url} was sent successfully"
+        @logger.debug response
         JSON.parse response
       end
 
       def send_put_request(url, params)
         @resource[url].put prepared_params(params)
-        RestClient.log.info "PUT request to #{url} was sent successfully"
+        @logger.info "PUT request to #{url} was sent successfully"
       end
 
       def send_post_request(url, params)
         @resource[url].post prepared_params(params)
-        RestClient.log.info "POST request to #{url} was sent successfully"
+        @logger.info "POST request to #{url} was sent successfully"
       end
 
       def send_delete_request(url, params = nil)
         @resource[url].delete params: params
-        RestClient.log.info "DELETE request to #{url} was sent successfully"
+        @logger.info "DELETE request to #{url} was sent successfully"
       end
 
       private
